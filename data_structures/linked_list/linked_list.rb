63c605badc5fe2ab7c568476b91397ebbf890527
gregoryromanov <gregoryromanov@gmail.com>
2021-01-22 19:21:48 +0300

    updated linked_list

--- data_structures/linked_list/linked_list.rb
+++ data_structures/linked_list/linked_list.rb
@@ -10,6 +10,7 @@ end
 class LinkedList
   def initialize(values = nil)
     @head = nil
+    @tail = nil
     @size = 0
 
     values.each(&->(value) { unshift value }) unless values.nil?
@@ -18,12 +19,12 @@ class LinkedList
   def unshift(value)
     new_node = Node.new value
     if @head.nil?
-      @head = new_node
+      @head = @tail = new_node
     else
       new_node.next = @head
       @head = new_node
     end
-    @size += 1
+    increment_size
   end
 
   def shift
@@ -31,9 +32,38 @@ class LinkedList
 
     removed = @head
     @head = @head.next
-    @size -= 1
 
     removed
+  end
+
+  def push(value)
+    new_node = Node.new value
+    if @head.nil?
+      @head = @tail = new_node
+    else
+      @tail = @tail.next = new_node
+    end
+    increment_size
+  end
+
+  def inset_into(position, value)
+    if position <= 0 then unshift value
+    elsif position >= @size then push value
+    else
+      new_node = Node.new value
+      place = 0
+      target = @head
+
+      until place == position - 1
+        target = target.next
+        place += 1
+      end
+
+      next_node = target.next
+      target.next = new_node
+      new_node.next = next_node
+      increment_size
+    end
   end
 
   def all
@@ -47,19 +77,35 @@ class LinkedList
     result
   end
 
-  attr_reader :size, :head
+  attr_reader :size, :head, :tail
+
+  private
+
+  def increment_size
+    @size += 1
+  end
+
+  def decrement_size
+    @size -= 1
+  end
 
   alias first head
+  alias last tail
 end
 
-list = LinkedList.new ['Alex Lukashenko', 'Vladimir Putin']
+list = LinkedList.new [3, 2]
 
-list.unshift 'Dima Petrov'
-list.unshift 'Oleg Sidorov'
-list.unshift 'Olga Buzovo'
+list.unshift 1
+list.unshift 2
+list.unshift 5
+list.push 12
+list.push 8
 
 pp list.size
 pp list.first.value
+pp list.last.value
 pp list.shift.value
+
+list.inset_into 10, 20
 
 pp list.all
